fd88faa59bde65a2964aad623d83eba9f94b6d48
Alyce Adams <alyad101@gmail.com>
2021-03-13 20:58:22 -0700

    Added to code journal and uploaded star wars jsons

--- code-journal.js
+++ code-journal.js
@@ -38,5 +38,65 @@ let myBool = false;
 
 let myArray = []; // empty array
 
+//              0       1        2       3      4
 let myArray2 = [23, "string", myBool, ANSWER, true]; // arrays can contain any object - variables, numbers, constants, strings, booleans, other arrays, etc.
+// Arrays are indexed. Everything in the array has an index number (where it sits in the array). 23 is at index 0, "string" is at index 1, etc. Arrays are a 0-base index.
+
+let secondElement = myArray2[1]; // returns "string" (if the position doesn't exist in an array, will return undefined)
+
+let lastItem = myArray2[myArray2.length - 1]; // returns last item in the array (need the -1 because of 0 index)
+
+myArray2.push('Alyce'); // adds 'Alyce' to the end of myArray2
+
+myArray2.pop; // removes the last item in the array
+
+myArray2.unshift('beginning'); // adds 'beginning' to index 0 within myArray2
+
+myArray2.shift; // removes the first item in the array
+
+let myLongString = "asdfjkl;1235kl;lkjdanivioowlndiojanwe34345346432";
+
+myLongString.length; // returns 48. Strings are basically just arrays of characters
+
+// Object - everything in Javascript is essentially an object. Javascript is an object-based language (different from object-oriented language)
+
+let minObject = {}; // {} are an indicator of the body of an object or the body of a function
+
+const myCar = {
+    make: 'Honda',
+    model: 'Element',
+    year: '2008',
+    color: 'black',
+    vin: '9809709870q12342asdfj2343', // dangling comma - a lot of programmers prefer adding it
+}
+// order of key properties doens't matter
+
+myCar.numDoors = 4; // adds property numDoors to the object myCar
+
+const anotherObject = {
+    words: ['string', 'hello', 'world'],
+    car: {
+        make: 'Ford',
+        model: 'Focus',
+    },
+    awesome: true,
+}; // Object properties can include entire objects within the object
+
+// Functions - little engines. It takes inputs and when it runs, it can produce outputs
+
+function myFunction() { // () indicate a function is involved
+    return 'Hello World!';
+};
+
+myFunction(); // calls the function - it will return the output of the function
+myFunction; // calls the actual function (won't run the function)
+
+function sumTwoThings(one, two) { // one and two are parameters or arguments
+    return one + two;
+};
+
+sumTwoThings(2, 2); // returns 4
+sumTwoThings('2', '2'); // returns '22' because of concatenation - combining two string characters together.
+sumTwoThings('4', 5); // returns '45' because of type coersion - turns 5 into a string.
+sumTwoThings('8', true); // returns '8true' because of type coersion - turns true into a string.
 
