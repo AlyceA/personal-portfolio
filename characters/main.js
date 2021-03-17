dbc8ea4de9c8723c4d7b1216ec8a5d170a5307ec
Alyce Adams <alyad101@gmail.com>
2021-03-17 13:50:13 -0600

    added a button that populates male characters

--- characters/main.js
+++ characters/main.js
@@ -2,6 +2,17 @@ import { people } from '../data/people.js';
 
 const mainContent = document.querySelector('main');
 
+const mainHeader = document.createElement('header');
+
+const maleButton = document.createElement('button');
+maleButton.textContent = "Male Characters";
+maleButton.addEventListener('click', () => {
+    populateDOM(maleCharacters)
+});
+
+mainHeader.appendChild(maleButton);
+document.body.insertBefore(mainHeader, mainContent);
+
 const maleCharacters = people.filter(person => person.gender === "male");
 const femaleCharacters = people.filter(person => person.gender === "female");
 const nbCharacters = people.filter(person => {
@@ -10,19 +21,21 @@ const nbCharacters = people.filter(person => {
     }
 });
 
-people.forEach(person => {
-    const charFigure = document.createElement('figure');
-    const charImage = document.createElement('img');
-    let charNum = getLastNumber(person.url);
-    charImage.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
-    const charCaption = document.createElement('figcaption');
+function populateDOM(characters) {
+    characters.forEach(person => {
+        const charFigure = document.createElement('figure');
+        const charImage = document.createElement('img');
+        let charNum = getLastNumber(person.url);
+        charImage.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`;
+        const charCaption = document.createElement('figcaption');
 
-    charCaption.textContent = person.name;
+        charCaption.textContent = person.name;
 
-    charFigure.appendChild(charImage);
-    charFigure.appendChild(charCaption);
-    mainContent.appendChild(charFigure);
-})
+        charFigure.appendChild(charImage);
+        charFigure.appendChild(charCaption);
+        mainContent.appendChild(charFigure);
+    })
+}
 
 function getLastNumber(url) {
     let end = url.lastIndexOf('/')
