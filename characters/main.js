acd53735581e2c319d4e856589d71d9a0caebc02
Alyce Adams <alyad101@gmail.com>
2021-03-24 02:17:28 -0600

    added star field to figure out how to add grid later

--- characters/main.js
+++ characters/main.js
@@ -33,3 +33,31 @@ function getLastNum(url) {
     }
     return url.slice(start, end)
 }
+
+function addStarField(element, numStars) {
+    element.style.setProperty('background-color', 'black')
+    for (let i=0; i < numStars; i++) {
+        let star = document.createElement('div')
+        star.style.setProperty('width', '2px')
+        star.style.setProperty('height', '2px')
+        star.style.setProperty('background-color', 'white')
+
+        let xy = getRandomPosition()
+        star.style.left = `${xy[0]}px`
+        star.style.top = `${xy[1]}px`
+
+        star.style.setProperty('position', 'absolute')
+
+        element.appendChild(star)
+    }
+}
+
+function getRandomPosition() {
+    let y = document.body.scrollHeight
+    let x = document.body.scrollWidth
+    let randomY = Math.floor(Math.random() * y)
+    let randomX = Math.floor(Math.random() * x)
+    return [randomX, randomY]
+}
+
+addStarField(document.querySelector('body'), 400);
