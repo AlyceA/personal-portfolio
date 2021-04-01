29047e000c4f7be7f10d7416d4056084ca2f8468
Alyce Adams <alyad101@gmail.com>
2021-03-31 22:19:00 -0600

    cleaned up the code and comments

--- planets/main.js
+++ planets/main.js
@@ -2,12 +2,9 @@ import { planets } from '../data/planets.js';
 
 const mainContent = document.querySelector('main');
 const screen = document.querySelector('#screen');
-const planetFigure = document.querySelectorAll('.planet');
 const buttonLeft = document.querySelector('#buttonLeft');
 const buttonRight = document.querySelector('#buttonRight');
-const mainPlanet = document.querySelector('#mainPlanet');
-const leftPlanet = document.querySelector('#leftPlanet');
-const rightPlanet = document.querySelector('#rightPlanet');
+const displayedPlanet = document.querySelector('#displayedPlanet');
 
 let currentIndex = 15;
 
@@ -29,44 +26,13 @@ function decrease() {
 }
 
 function populateDOM(planetIndex) {
-  // populateLeft(planets[planetIndex - 1]);
   populateMain(planets[planetIndex]);
-  // populateRight(planets[planetIndex + 1]);
 }
-
-// function populateLeft() {
-//   const planetImg = document.createElement('img');
-//   planetImg.src = `https://starwars-visualguide.com/assets/img/planets/${currentIndex - 1}.jpg`;
-
-//   addLeftClass;
-//   planetFigure.appendChild(planetImg);
-//   screen.appendChild(planetFigure);
-// }
-
-// function populateRight() {
-//   const planetImg = document.createElement('img');
-//   planetImg.src = `https://starwars-visualguide.com/assets/img/planets/${planetNum}.jpg`;
-
-//   addRightClass;
-//   planetFigure.appendChild(planetImg);
-//   screen.appendChild(planetFigure);
-// }
 
 function populateMain(planet) {
-  mainPlanetName.textContent = planet.name;
-  mainPlanetClimate.textContent = planet.climate + " planet";
-  mainPlanet.src = `https://starwars-visualguide.com/assets/img/planets/${currentIndex}.jpg`;
-  // const planetImg = document.createElement('img');
-  // const planetName = document.createElement('div');
-  // planetImg.src = `https://starwars-visualguide.com/assets/img/planets/${planet.id}.jpg`;
-
-  // addMainClass;
-  // planetName.textContent = planet.name;
-  // planetFigure.appendChild(planetImg);
-  // planetFigure.appendChild(planetName);
-  // screen.appendChild(planetFigure);
+  planetName.textContent = planet.name;
+  planetClimate.textContent = planet.climate + " planet";
+  displayedPlanet.src = `https://starwars-visualguide.com/assets/img/planets/${currentIndex}.jpg`;
 }
 
-// // display 3 planets at a time, middle one larger (flexgrow/shrink) with name below it, when the arrow on the left is clicked, the planet on the left moves to the middle position and vice-versa for the right arrow. When there are no more planets available, the arrow should disappear and there should only be two planets on the screen.
-
-// // if there's time, can add rotation period, orbital period, diameter, climate, gravity, terrain, surface water, population under middle planet
+// Planet information screen with a "nav" or "list" of planets on the left-hand side. Planet information will include planet image, planet name, planet climate, rotational period, orbital period, diameter, gravity, surface water, terrain, and population.
