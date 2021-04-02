d9efc19704cd44a5a79975d1ec2bc4fcc1b08aa7
Alyce Adams <alyad101@gmail.com>
2021-04-01 18:24:18 -0600

    added additional planet information

--- planets/main.js
+++ planets/main.js
@@ -32,7 +32,15 @@ function populateDOM(planetIndex) {
 function populateMain(planet) {
   planetName.textContent = planet.name;
   planetClimate.textContent = planet.climate + " planet";
+  planetPopulation.textContent = 'population ' + planet.population;
   displayedPlanet.src = `https://starwars-visualguide.com/assets/img/planets/${currentIndex}.jpg`;
+  planetRotationalPeriod.textContent = 'rotational period: ' + planet.rotation_period;
+  planetOrbitalPeriod.textContent = 'orbital period: ' + planet.orbital_period;
+  planetDiameter.textContent = 'diameter: ' + planet.diameter;
+  planetGravity.textContent = 'gravity: ' + planet.gravity;
+  surfaceWater.textContent = 'surface water: ' + planet.surface_water;
+  terrain.textContent = 'terrain: ' + planet.terrain;
+  climate.textContent = 'climate: ' + planet.climate;
 }
 
 // Planet information screen with a "nav" or "list" of planets on the left-hand side. Planet information will include planet image, planet name, planet climate, rotational period, orbital period, diameter, gravity, surface water, terrain, and population.
