6bc654b2a4d53dd6493f30896e7ca17281f0ad93
Alyce Adams <alyad101@gmail.com>
2021-04-22 15:38:55 -0600

    created actual cards and added bulbasaur img

--- pokemon/main.js
+++ pokemon/main.js
@@ -28,7 +28,31 @@ function loadPage() {
 
 function populatePokeCard(singlePokemon) {
     let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
     let pokeCard = document.createElement('div')
-    
-    console.log(singlePokemon)
+    pokeCard.className = 'card'
+
+    pokeCard.appendChild(populateCardFront(singlePokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+    let pokeFront = document.createElement('div')
+    pokeFront.className = 'card__face card__face--front'
+    let frontLabel = document.createElement('p')
+    frontLabel.textContent = pokemon.name
+    let frontImg = document.createElement('img')
+    frontImg.src = `images/001.png`
+    pokeFront.appendChild(frontImg)
+
+    pokeFront.appendChild(frontLabel)
+    return pokeFront
+}
+
+function populateCardBack(pokemon) {
+    let pokeBack = document.createElement('div')
+    pokeBack.className = 'card__face card__face--back'
+    let backLabel = document.createElement('p')
+    backLabel.textContent = 'back of card'
 }
